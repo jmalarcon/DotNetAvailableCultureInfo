88e4400c5789d7bb7de4e3832e9df25aa7720451
José M. Alarcón <jmalarcon@jasoft.org>
2020-09-03 08:10:26 +0200

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace DotNetAvailableCulturesInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputHelper oh;
+            if (args.Length > 0 && args[0] == "")
+                oh = new OutputHelper(Console.Out);
+            else
+                oh = new OutputHelper(Console.Out);
+
+            oh.WriteLine("CULTURE,ISO,ISO,WIN,DISPLAYNAME,ENGLISHNAME");
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                oh.Write("{0, -7}", ci.Name);
+                oh.Write(" {0, -3}", ci.TwoLetterISOLanguageName);
+                oh.Write(" {0, -3}", ci.ThreeLetterISOLanguageName);
+                oh.Write(" {0, 3}", ci.ThreeLetterWindowsLanguageName);
+                oh.Write(" {0, -40}", ci.DisplayName);
+                oh.WriteLine("{0, -40}", ci.EnglishName);
+            }
+        }
+
+    }
+
+    // Used interchangeably to output information thru different streams
+    internal class OutputHelper
+    {
+        private readonly TextWriter _strm;
+
+        internal OutputHelper(TextWriter strm)
+        {
+            _strm = strm;
+        }
+
+        internal void Write(string info, params object[] arg)
+        {
+            _strm.Write(info, arg);
+        }
+
+        internal void WriteLine(string info, params object[] arg)
+        {
+            _strm.WriteLine(info, arg);
+        }
+    }
+}
